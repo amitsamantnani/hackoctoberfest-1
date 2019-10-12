bc3d114d1365878ed072cbb1f13f8f3498545931
ValenTheRed <56402118+ValenTheRed@users.noreply.github.com>
2019-10-12 08:50:11 +0530

    Update age_calcu.cpp
    C++17 compliant code.
    Some error checking capability.
    Updated logic.

--- age_calcu.cpp
+++ age_calcu.cpp
@@ -1,21 +1,56 @@
-#include<iostream.h>
+#include<iostream>
 #include<conio.h>
-void main()
+
+int main(void)
 {
-int birthmonth,birthyear;
-int currentmonth,currentyear;
-int agey,agem;
-cout<<"\n\n\t\t\tRaje's Age Calculator\n\n";
-cout<<"Enter Your Birth Year(Eg:1989):";
-cin>>birthyear;
-cout<<"\n\nEnter Your Birth Month(Eg:7):";
-cin>>birthmonth;
-cout<<"\nEnter The Current Month(Eg:7):";
-cin>>currentmonth;
-cout<<"\nEnter The Current Year(Eg:2010):";
-cin>>currentyear;
-agey=currentyear-birthyear;
-agem=12-birthmonth;
-cout<<"\n\n\t\tYour Age is "<<agey<<" Years And "<<agem<<" Months ";
-getch();
+	int birthmonth, birthyear;
+	int currentmonth, currentyear;
+	int agey, agem;
+
+	std::cout << "\n\n\t\t\tRaje's Age Calculator\n\n";
+  
+	std::cout << "Enter your birth year(Eg: 1989):";
+	std::cin >> birthyear;
+	
+	std::cout << "Enter your birth month(Eg: 7):";
+	std::cin >> birthmonth;
+	//Check whether month = [1...12]
+	if (birthmonth < 1 || birthmonth > 12) {
+		std::cout << "Invalid intput! Month should be between 1 and 12.";
+		return 0;
+	}
+
+	std::cout << "\nEnter the current year(Eg: 2010):";
+	std::cin >> currentyear;
+	if (birthyear > currentyear) {
+		std::cout << "Invalid input! Current year must be more than birth year.";
+		return 0;
+	}
+
+
+	std::cout << "Enter the current month(Eg: 7):";
+	std::cin >> currentmonth;
+	//Check whether month = [1...12]
+	if (birthmonth < 1 || birthmonth > 12) {
+		std::cout << "Invalid input! Month should be inbetween 1 and 12.";
+		return 0;
+	}
+
+ 	//Calculating age
+	if (birthmonth > currentmonth && birthyear >= currentyear) {
+		std::cout << "Invalid input! Current date must be more than birth date.";
+		return 0;
+	}
+	else if (birthmonth > currentmonth) {
+		//birthmonth > currentmonth => age is one year less than the difference
+		agey = currentyear - birthyear - 1;
+		agem = 12 - (birthmonth - currentmonth);
+	}
+	else {
+		agey = currentyear - birthyear;
+		agem = currentmonth - birthmonth;
+	}
+
+	std::cout << "\n\t\tYour Age is: " << agey << " years and " << agem << " months.";
+	//getch();
 }
